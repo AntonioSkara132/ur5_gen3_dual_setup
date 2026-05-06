bf3e8777e73a8e0bb1888e00349f82421f3cb9de
AntonioSkara132 <conio1240@gmail.com>
2026-05-06 15:44:17 +0000

    changes

--- ur5/ur_dual_bringup/launch/dual_moveit2.launch.py
+++ ur5/ur_dual_bringup/launch/dual_moveit2.launch.py
@@ -123,6 +123,8 @@ def launch_setup(context, *args, **kwargs):
     #     ],
     #     #namespace=robot_namespace,
     # )
+
+
     
     robot_description = {"robot_description": robot_description_content}
     
@@ -256,6 +258,7 @@ def launch_setup(context, *args, **kwargs):
                 "use_sim_time": True,
                 #"publish_robot_description_semantic": publish_robot_description_semantic,
                 "robot_description": robot_description_content,
+                "publish_robot_description_semantic": True,
             },
         ],
         #remappings=[('/robot_description', rd_path), ('/joint_states', js_path)],
