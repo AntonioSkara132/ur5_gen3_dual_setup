ccc41c861fb3259127550ea2f00d9e69c7bd89f1
AntonioSkara132 <conio1240@gmail.com>
2026-05-11 10:18:58 +0000

    changed nosac model

--- ur5/ur_dual_bringup/launch/ur_control.launch.py
+++ ur5/ur_dual_bringup/launch/ur_control.launch.py
@@ -177,18 +177,18 @@ def launch_setup(context):
         arguments=["-d", rviz_config_file],
     )
 
-    trajectory_until_node = Node(
-        package="ur_robot_driver",
-        executable="trajectory_until_node",
-        name="trajectory_until_node",
-        output="screen",
-        parameters=[
-            {
-                "motion_controller": initial_joint_controller,
-            },
-        ],
-        namespace=robot_namespace,
-    )
+    # trajectory_until_node = Node(
+    #     package="ur_robot_driver",
+    #     executable="trajectory_until_node",
+    #     name="trajectory_until_node",
+    #     output="screen",
+    #     parameters=[
+    #         {
+    #             "motion_controller": initial_joint_controller,
+    #         },
+    #     ],
+    #     namespace=robot_namespace,
+    # )
 
     # Spawn controllers
     def controller_spawner(controllers, active=True):
@@ -254,7 +254,7 @@ def launch_setup(context):
         urscript_interface,
         rsp,
         rviz_node,
-        trajectory_until_node,
+        #trajectory_until_node,
     ] + controller_spawners
 
     return nodes_to_start
